48414202881657ed4b3d1afb390d0723699e2159
Rockfordgun <anton@sunstroke.org>
2023-04-16 12:52:19 +0200

    navigation Started

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const products = [
     status: "active",
     short_description:
       "The '90s era? Yeah, that was the bomb. Nike AF-1 style? A favourite forever.",
-    img: "../img/shoes/Thumbnails/Nike_force_l1.jpg",
+    img: "./img/shoes/Thumbnails/Nike_force_l1.jpg",
     price: "R2 299.00",
   },
 
@@ -17,7 +17,7 @@ const products = [
     category: "shoes",
     short_description:
       "Built to Last, Crafted to Turn HeadsThe Nike Men's Monarch IV is the sneaker of the '80s ",
-    img: "../img/shoes/Thumbnails/Monarch.jpg",
+    img: "./img/shoes/Thumbnails/Monarch.jpg",
     price: "R2 999.00",
   },
 
@@ -28,7 +28,7 @@ const products = [
     category: "sunglasses",
     short_description:
       " selection of sunglasses inspired by race car design with Scuderia Ferrari details and a choice of Polarized lenses",
-    img: "../img/sunglasses/thumbnails/ferrari.jpg",
+    img: "./img/sunglasses/thumbnails/ferrari.jpg",
     price: "R4 120.00",
   },
 
@@ -38,7 +38,7 @@ const products = [
     category: "sunglasses",
     short_description:
       "Mens Split Shot with Matte Tortoise frame and Prizm Tungsten Polarized lenses",
-    img: "../img/sunglasses/thumbnails/slingshotT.jpg",
+    img: "./img/sunglasses/thumbnails/slingshotT.jpg",
     price: "R3 450.00",
   },
 
@@ -49,7 +49,7 @@ const products = [
     category: "accesories",
     short_description:
       "You can't improve on a classic, but we've refined our Derrick bifold with modern details",
-    img: "../img/accesorries/thumbnail/fossilwallet.jpg",
+    img: "./img/accesorries/thumbnail/fossilwallet.jpg",
     price: "R1 399.00",
   },
 
@@ -59,7 +59,7 @@ const products = [
     category: "accesories",
     short_description:
       "Armani Exchange's 42mm watch features a black sunray dial, multifunction movement and black stainless steel bracelet.",
-    img: "../img/accesorries/thumbnail/armaniwatch.jpg",
+    img: "./img/accesorries/thumbnail/armaniwatch.jpg",
     price: "R4 599.00",
   },
 
@@ -70,7 +70,7 @@ const products = [
     category: "mens",
     short_description:
       "Regular-rise jeans cut to a tapered fit in dark-blue supreme-movement denim by BOSS Menswear.",
-    img: "../img/mens/thumbnails/hugojeans.jpg",
+    img: "./img/mens/thumbnails/hugojeans.jpg",
     price: "R2 700.00",
   },
 
@@ -80,7 +80,7 @@ const products = [
     category: "mens",
     short_description:
       "A sporty polo shirt by BOSS Menswear. Featuring a rhinestone BOSS logo",
-    img: "../img/mens/thumbnails/hugoshirt.jpg",
+    img: "./img/mens/thumbnails/hugoshirt.jpg",
     price: "R2 100.00",
   },
 
@@ -92,7 +92,7 @@ const products = [
     category: "woman",
     short_description:
       "Ruby red Sophia slip silk dress from Gilda & Pearl featuring a bias cut finish, a V-neck",
-    img: "../img/womans/thumbnail/womandress.jpg",
+    img: "./img/womans/thumbnail/womandress.jpg",
     price: "R8 018.55",
   },
 
@@ -102,7 +102,7 @@ const products = [
     category: "woman",
     short_description:
       "You can't go wrong with a simple black roll neck jumper now can you? Especially when it's Gucci. ",
-    img: "../img/womans/thumbnail/turtle.jpg",
+    img: "./img/womans/thumbnail/turtle.jpg",
     price: "R17 345.98",
   },
 ];
@@ -164,3 +164,15 @@ function displayMenuItems(menuItems) {
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
 }
+
+/*Daily Deals Timer*/
+
+let timeLeft = 10;
+
+const countDown = () => {
+  timeLeft--;
+
+  if (timeLeft === 0) {
+    console.log("hello there");
+  }
+};
